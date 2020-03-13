1814ecd1c475583aecc5f1bc612ef8b34df2170d
stban2703 <s.t.ban1401@gmail.com>
2020-03-13 13:43:16 -0500

    Primer commit

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("main.Main");
+
+	}
+
+	public void settings() {
+		size(500, 500);
+	}
+
+	public void setup() {
+
+	}
+
+	public void draw() {
+		background(255);
+
+	}
+
+}
